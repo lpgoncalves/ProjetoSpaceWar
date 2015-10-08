0fa1c08fc6036e26ba1856bf87d6129082c8c59c
Matheus Galvano <matheusgalvano19@gmail.com>
2015-10-08 09:26:29 -0300

    Atualização 09:26, dia 08/10/2015

---
+++ ProjetoSpaceWar/src/JanelaPrincipal.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class JanelaPrincipal extends JFrame {
+	
+	public JanelaPrincipal() {
+	       
+		setTitle("Space War");
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setLocationRelativeTo(null);
+	    setSize(600,600);
+	    setVisible(true);
+	       
+	}
+       
+}
